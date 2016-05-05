912d3154e90ebd3f4cfd64549dff2291d722fffb
ct85711 <ct85711@gmail.com>
2016-05-05 14:06:30 -0500

    Updated files

--- App_Code/Customer.cs
+++ App_Code/Customer.cs
@@ -8,42 +8,10 @@ using System.Web;
 /// </summary>
 public class Customer
 {
-    private static int UserIDCount = 100;
-
-    //properties
-    public int UserID { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Address { get; set; }
-    public string City { get; set; }
-    public string State { get; set; }
-    public string Zip { get; set; }
-    public string Phone { get; set; }
-    public string EmailAddress { get; set; }
-    public string UserName { get; set; }
-    public string Password { get; set; }
-
-    //constructors
 	public Customer()
 	{
-		
+		//
+		// TODO: Add constructor logic here
+		//
 	}
-    public Customer(int id, string fname, string lname, string address,
-        string city, string state, string zipCode, string email,
-        string userName, string passwd)
-    {
-        this.UserID = id;
-        this.FirstName = fname;
-        this.LastName = lname;
-        this.Address = address;
-        this.City = city;
-        this.State = state;
-        this.Zip = zipCode;
-        this.EmailAddress = email;
-        this.UserName = userName;
-        this.Password = passwd;
-    }
-
-    //methods
-    
 }
---
+++ CreateAccount/CreateAccount.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="CreateAccount.aspx.cs" Inherits="CreateAccount_Account" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceholder" Runat="Server">
+</asp:Content>
+
