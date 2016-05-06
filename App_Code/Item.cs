da8bf2c9d86a2ca61fd946d39b6a121aba93891e
jbw521 <jbw521@gmail.com>
2016-05-05 20:19:55 -0500

    Added Item class.

---
+++ App_Code/Item.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Item
+/// </summary>
+public class Item
+{
+    public int artId { get; set; }
+    public int supplierId { get; set; }
+    public string artName { get; set; }
+    public string artType { get; set; }
+    public decimal price { get; set; }
+    public string description { get; set; }
+
+
+	public Item()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public Item (int artId, int supplierId, string artType, decimal price, string description)
+    {
+        this.artId = artId;
+        this.supplierId = supplierId;
+        this.artName = artName;
+        this.price = price;
+        this.description = description;
+
+
+    }
+}
