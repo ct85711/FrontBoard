2280341482e668c54dcbba6de55e96e0531604ff
ct85711 <ct85711@gmail.com>
2016-05-10 13:27:19 -0500

    Fixed Customer Class

--- App_Code/Customer.cs
+++ App_Code/Customer.cs
@@ -8,10 +8,42 @@ using System.Web;
 /// </summary>
 public class Customer
 {
-	public Customer()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+    private static int UserIDCount = 100;
+
+    //properties
+    public int UserID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Zip { get; set; }
+    public string Phone { get; set; }
+    public string EmailAddress { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+
+    //constructors
+    public Customer()
+    {
+
+    }
+    public Customer(int id, string fname, string lname, string address,
+        string city, string state, string zipCode, string email,
+        string userName, string passwd)
+    {
+        this.UserID = id;
+        this.FirstName = fname;
+        this.LastName = lname;
+        this.Address = address;
+        this.City = city;
+        this.State = state;
+        this.Zip = zipCode;
+        this.EmailAddress = email;
+        this.UserName = userName;
+        this.Password = passwd;
+    }
+
+    //methods
+
 }
