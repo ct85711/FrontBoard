c51561a1db81922d628d3cd5246ae4fa844cf3ac
Craig Pounds <nweaver@windstream.net>
2016-05-12 22:10:45 -0500

    Added code to hide/show Login controls to MasterPage.mastercs

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -10,13 +10,24 @@ public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-     
-
+        // Hide Log In controls when we are logged in
+        if (HttpContext.Current.Session["createAccount"] != null)
+        {
+            lblUserName.Visible = false;
+            txtUserName.Visible = false;
+            lblPassWord.Visible = false;
+            txtPassword.Visible = false;
+            btnLogin.Visible = false;
+            btnCreateAcct.Visible = false;
+            btnLogOut.Visible = true;
+        }
     }
+
     protected void btnCreateAcct_Click(object sender, EventArgs e)
     {
         Response.Redirect("~/CreateAccount/CreateAccount.aspx");
     }
+
     protected void btnLogin_Click(object sender, EventArgs e)
     {
         if (!string.IsNullOrWhiteSpace(txtUserName.Text) && !string.IsNullOrWhiteSpace(txtPassword.Text))
@@ -41,5 +52,21 @@ public partial class MasterPage : System.Web.UI.MasterPage
                 Response.Write("<script>alert('" + msg + "')</script>");
             }
         }
+    }
+
+    protected void btnLogOut_Click(object sender, EventArgs e)
+    {
+        // Clear user info
+        Session.Remove("createAccount");
+
+        // Show Log In controls
+        lblUserName.Visible = true;
+        txtUserName.Visible = true;
+        lblPassWord.Visible = true;
+        txtPassword.Visible = true;
+        btnLogin.Visible = true;
+        btnCreateAcct.Visible = true;
+        btnLogOut.Visible = false;
+        Response.Redirect("~/Default.aspx");
     }
 }
