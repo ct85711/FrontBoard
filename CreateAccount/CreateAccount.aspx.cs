d0d0c801d40c04c72775f4b000355d3792e14ffe
ct85711 <ct85711@gmail.com>
2016-05-31 14:10:19 -0500

    Removed the commented out code that wasn't needed anymore.

--- CreateAccount/CreateAccount.aspx.cs
+++ CreateAccount/CreateAccount.aspx.cs
@@ -42,47 +42,6 @@ public partial class CreateAccount : System.Web.UI.Page
         }
     }
 
-    //protected void btnCreateAccount_Click(object sender, EventArgs e)
-    //{
-    //    string firstName = FormView1.;
-    //    string lastName = LastNameTextBox.Text;
-    //    string address = txtAddress.Text;
-    //    string city = txtCity.Text;
-    //    string state = txtState.Text;
-    //    string zipcode = txtZip.Text;
-    //    string phone = txtPhone.Text;
-    //    string email = txtEmail.Text;
-    //    string username = txtUsername.Text;
-    //    string password = txtPassword.Text;
-
-    //    try
-    //    {
-    //        var cust = FrontBoardDA.GetCustomerByUsername(username);
-
-    //        if (cust == null)
-    //        {
-    //            lblErrorMessage.Text = string.Empty;
-
-    //            Customer newCustomer = new Customer(firstName, lastName, address, city, state, zipcode, phone, email, username, password);
-
-    //            FrontBoardDA.InsertCustomer(newCustomer);
-
-    //            // TODO - Create a Customer in the Database
-    //            Session["createAccount"] = newCustomer;
-
-    //            Server.Transfer("~/Default.aspx");
-    //        }
-    //        else
-    //        {
-    //            lblErrorMessage.Text = "That username already exists. Please choose a different one.";
-    //        }
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        lblErrorMessage.Text = "Sorry, there was an error connecting to the database. Try again later.";
-    //    }
-    //}
-
     protected void btnChangePasswod_Click(object sender, EventArgs e)
     {
         Response.Redirect("~/CreateAccount/ChangePassword.aspx");
